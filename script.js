dc95e72e4c26d8cb39fbc6c12b815e276e57c1ce
Rino <lukatevzadze43@gmail.com>
2024-04-18 13:40:30 +0400

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function updateTime() {
   document.getElementById("second").textContent = timeChecker.getSeconds();
 }
 
-let hour = timeChecker.GetHours();
+let hour = timeChecker.getHours();
 
 if (hour === 19 || hour > 19) {
   time.innerText = "Oh no its night, and plus its  AM!!";
